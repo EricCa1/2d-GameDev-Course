d894458bc75a5c035450828390205c51800f78b2
EricCa1 <ercai@s.sfusd.edu>
2024-04-29 13:56:45 -0700

    L5 Completed

--- M06.looting_workbook/practices/L5.P2.pop_the_balloon/balloon_pop.gd
+++ M06.looting_workbook/practices/L5.P2.pop_the_balloon/balloon_pop.gd
@@ -1,15 +1,11 @@
 extends Area2D
 
-
-func open() -> void:
-	
-	input_pickable = false
-	
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 	var event_is_mouse_release: bool = (
 		event is InputEventMouseButton
-		and event.button_index == MOUSE_BUTTON_LEFT and
-		event.is_pressed() )
+		and event.button_index == MOUSE_BUTTON_LEFT
+		and event.is_released() #
+	)
 
 	if event_is_mouse_release:
-		open()
+		queue_free()  # pass
